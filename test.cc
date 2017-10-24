bc9f1d12531a9a89df32057b5bd14d19c4fdee81
Norland-w <norland.xw@gmail.com>
2017-10-24 21:13:49 +0800

    add a.out test.cc

---
+++ test.cc
@@ -0,0 +1,14 @@
+///
+/// @file    test.c
+/// @author  Norland(norland.xw@gmail.com)
+/// @date    2017-10-24 20:35:15
+///
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	std::cout << "helloworld!"<<endl;
+}
